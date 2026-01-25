65669ecd2711ea05b3f8131cfdf0298fd987931b
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2026-01-25 14:58:31 +0700

    feat: implement AuthGuard to check for public routes using metadata

--- src/application/common/auth/AuthGuard.ts
+++ src/application/common/auth/AuthGuard.ts
@@ -6,6 +6,8 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
+import { IS_PUBLIC_KEY } from '../Metadata';
+import { Reflector } from '@nestjs/core';
 
 interface JwtPayload {
   sub: string;
@@ -20,9 +22,21 @@ interface JwtPayload {
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass()
+    ]);
+    if (isPublic) {
+      // 💡 See this condition
+      return true;
+    }
+
     const request = context.switchToHttp().getRequest<Request>();
     const token = this.extractTokenFromHeader(request);
     if (!token) {

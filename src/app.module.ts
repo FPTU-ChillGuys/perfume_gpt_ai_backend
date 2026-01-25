94bd952ace2a274c25889ecd524fa54df4e89d1f
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2026-01-25 15:02:20 +0700

    feat: add AuthGuard as a global provider for route protection

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,8 @@ import defineConfig from '../mikro-orm.config';
 import { ProviderModule } from './infrastructure/modules/provider.module';
 import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthGuard } from './application/common/auth/AuthGuard';
+import { APP_GUARD } from '@nestjs/core';
 @Module({
   imports: [
     AutomapperModule.forRoot({
@@ -20,11 +22,18 @@ import { JwtModule } from '@nestjs/jwt';
     ConfigModule.forRoot(),
     ProviderModule,
     JwtModule.register({
+      global: true,
       secret: process.env.PUBLIC_KEY,
       signOptions: { algorithm: 'RS256' }
     })
   ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard
+    }
+  ]
 })
 export class AppModule {}

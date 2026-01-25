f02ac3c664ed7b20400415fe7d8c4c8255688599
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2026-01-25 14:35:16 +0700

    feat: add AuthGuard for JWT authentication and protect routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import defineConfig from '../mikro-orm.config';
 import { ProviderModule } from './infrastructure/modules/provider.module';
 import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
   imports: [
     AutomapperModule.forRoot({
@@ -17,7 +18,11 @@ import { ConfigModule } from '@nestjs/config';
       autoLoadEntities: true
     }),
     ConfigModule.forRoot(),
-    ProviderModule
+    ProviderModule,
+    JwtModule.register({
+      secret: process.env.PUBLIC_KEY,
+      signOptions: { algorithm: 'RS256' }
+    })
   ],
   controllers: [AppController],
   providers: [AppService]

160ef4175fd9474a58c47aeaabbad71e989173bd
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2026-01-25 12:03:22 +0700

    feat: add HttpExceptionFilter for centralized error handling and update main.ts to use the filter

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { apiReference } from '@scalar/nestjs-api-reference';
+import { HttpExceptionFilter } from './application/filters/http-error-handler.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -15,6 +16,8 @@ async function bootstrap() {
       content: document
     })
   );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(process.env.PORT ?? 3000);
 }

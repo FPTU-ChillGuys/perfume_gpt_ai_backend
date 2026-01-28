c021fe286e2ac22a874cbe0d3f27cec41513dc63
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2026-01-28 22:55:34 +0700

    feat: add ApiProperty decorators to AIReqResResponse fields

--- src/application/dtos/response/ai-request-response.response.ts
+++ src/application/dtos/response/ai-request-response.response.ts
@@ -1,10 +1,15 @@
 import { RequestType } from 'src/domain/enum/request-type.enum';
 import { CommonResponse } from './common/common.response';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class AIReqResResponse extends CommonResponse {
+  @ApiProperty()
   userId!: string;
+  @ApiProperty()
   requestType!: RequestType;
+  @ApiProperty()
   prompt!: string;
+  @ApiProperty()
   response!: string;
 
   constructor(init?: Partial<AIReqResResponse>) {

bba819b355cca3cad9bad8643666c4a59abd4586
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2026-02-04 20:20:57 +0700

    feat: update ReviewController to include query parameter for fetching reviews

--- src/api/controllers/review.controller.ts
+++ src/api/controllers/review.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { GetPagedReviewRequest } from 'src/application/dtos/request/get-paged-review.request';
+import { ReviewService } from 'src/infrastructure/servicies/review.service';
 
 @Controller('reviews')
 export class ReviewController {
 
+    constructor(
+        private readonly reviewService: ReviewService
+    ) {}
+
     //Get reviews
     @Get()
-    async getReviews() {
-        
+    async getReviews(@Query() request: GetPagedReviewRequest): Promise<any> {
+        return await this.reviewService.getAllReviews(request);
     }
-
-
 
     //Review summary
     

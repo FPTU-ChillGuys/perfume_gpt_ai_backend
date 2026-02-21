e89f5060d4ef8a4dda4ec309569f3d80a9e6f83f
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2026-02-21 17:26:46 +0700

    feat(prisma): update Products model to comment out Embedding field and enhance PrismaService configuration for MSSQL

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -5,7 +5,19 @@ import { PrismaMssql } from '@prisma/adapter-mssql';
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
-    const adapter = new PrismaMssql({ url: process.env.SQL_SERVER_DATABASE_URL });
+
+    const adapter = new PrismaMssql({
+      server: process.env.SQL_SERVER_DATABASE_SERVER ?? 'localhost',      
+      port: Number(process.env.SQL_SERVER_DATABASE_PORT),  
+      user: process.env.SQL_SERVER_DATABASE_USER,          
+      password: process.env.SQL_SERVER_DATABASE_PASSWORD,  
+      database: process.env.SQL_SERVER_DATABASE_NAME,      
+      options: {
+        encrypt: false,
+        trustServerCertificate: true,
+      }
+    });
+
     super({ adapter });
   }
 }
